2c9c0e78567eb3dec5850105d6420410bfe07335
Mark Wienk <markwienk@gmail.com>
2012-04-15 13:43:06 +0200

    Werkend maken

--- S2 - RAD/src/org/pahospital/www/radiologycallbackservice/RadiologyCallbackServiceStub.java
+++ S2 - RAD/src/org/pahospital/www/radiologycallbackservice/RadiologyCallbackServiceStub.java
@@ -232,7 +232,7 @@
                 public static final javax.xml.namespace.QName MY_QNAME = new javax.xml.namespace.QName(
                 "http://www.PAHospital.org/RadiologyCallbackService/",
                 "RadiologyReport",
-                "ns2");
+                "ns1");
 
             
 
@@ -494,7 +494,7 @@
 
         private static java.lang.String generatePrefix(java.lang.String namespace) {
             if(namespace.equals("http://www.PAHospital.org/RadiologyCallbackService/")){
-                return "ns2";
+                return "ns1";
             }
             return org.apache.axis2.databinding.utils.BeanUtil.getUniquePrefix();
         }
